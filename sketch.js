19d93d712e01306820c6e9b443506ea615ce1911
sellGoCrazyYT <73556906+sellGoCrazyYT@users.noreply.github.com>
2021-06-09 12:00:17 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,110 +1,45 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-const Render = Matter.Render;
-const Constraint=Matter.Constraint;
+const Constraint = Matter.Constraint;
 
-var treeObj, stoneObj,groundObject;
-var mango1,mango2,mango3,mango4,mango5,mango6,mango7,mango8,mango9,mango10,mango11,mango12;
-var world,boy;
+var engine, world;
+var box1, box2, box3,box4;
+var hero,monster,rope,ground;
 
-//Declare launcherObject and launchForce variable here
-
-
-function preload(){
-	boy=loadImage("images/boy.png");
-  }
+function preload() {
+  bg = loadImage("gamingbackground2.png");
+}
 
 function setup() {
-	createCanvas(1300, 600);
-	engine = Engine.create();
-	world = engine.world;
+  createCanvas(3000, 700);
+  engine = Engine.create();
+  world = engine.world;
 
-	stoneObj=new stone(235,420,30); 
+  ground = new Ground(600, 600, 1200, 20);
 
-	mango1=new mango(1100,100,30);
-  mango2=new mango(1170,130,30);
-	mango3=new mango(1010,140,30);
-	mango4=new mango(1000,70,30);
-	mango5=new mango(1100,70,30);
-	mango6=new mango(1000,230,30);
-	mango7=new mango(900,230,40);
-	mango8=new mango(1140,150,40);
-	mango9=new mango(1100,230,40);
-	mango10=new mango(1200,200,40);
-	mango11=new mango(1120,50,40);
-	mango12=new mango(900,160,40);
+  hero = new Hero(400,800,250);
+  rope = new Rope(hero.body, { x: 500, y: 50 });
+  monster = new Monster(1100,550,300);
 
-	treeObj=new tree(1050,580);
-	groundObject=new ground(width/2,600,width,20);
-  //create launcherObject here
+  box1 = new Box(600, 100, 70, 70);
+  box2 = new Box(900, 100, 70, 70);
+  box3 = new Box(900, 100, 70, 70);
+  box4 = new Box(900, 100, 70, 70);
 
-
-	Engine.run(engine);
 }
 
 function draw() {
+  background(bg);
+  Engine.update(engine);
+  ground.display();
+  box1.display();
+  box2.display();
+  box3.display();
+  box4.display()
 
-  background(230);
-  textSize(25);
-  text("Press Space to get a second Chance to Play!!",50 ,50);
-  image(boy ,200,340,200,300);
-  
+  hero.display();
+  rope.display();
+  monster.display();
 
-  treeObj.display();
-  stoneObj.display();
-  mango1.display();
-  mango2.display();
-  mango3.display();
-  mango4.display();
-  mango6.display();
-  mango7.display();
-  mango8.display();
-  mango9.display();
-  mango10.display();
-  mango11.display();
-  mango12.display();
-
-  stoneObj.display();
-  groundObject.display();
-  // display launcher object here
-    
-
-
-  detectollision(stoneObj,mango1);
-  detectollision(stoneObj,mango2);
-  detectollision(stoneObj,mango3);
-  detectollision(stoneObj,mango4);
-  detectollision(stoneObj,mango5);
-  detectollision(stoneObj,mango6);
-  detectollision(stoneObj,mango7);
-  detectollision(stoneObj,mango8);
-  detectollision(stoneObj,mango9);
-  detectollision(stoneObj,mango10);
-  detectollision(stoneObj,mango11);
-  detectollision(stoneObj,mango12);
 }
-
-//create mouseDragged function here
-
-
-//create mouseReleased function here
-
-
-//create keyPressed function here
-
-
-  function detectollision(lstone,lmango){
-
-  mangoBodyPosition=lmango.body.position
-  stoneBodyPosition=lstone.body.position
-  
-  var distance=dist(stoneBodyPosition.x, stoneBodyPosition.y, mangoBodyPosition.x, mangoBodyPosition.y)
-  	if(distance<=lmango.r+lstone.r)
-    {
-  	  Matter.Body.setStatic(lmango.body,false);
-    }
-
-  }
